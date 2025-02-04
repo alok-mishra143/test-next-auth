1c6abb160fc2e4344fca89f5729f79fcb99efd94
Alok Mishra <21se02cs042@ppsu.ac.in>
2025-02-04 15:49:39 +0530

    added custom ESlint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,15 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "no-console": "off", // Allows console logs
+      "no-unused-vars": "warn", // Warnings instead of errors
+      "@typescript-eslint/no-unused-vars": "warn", // Avoids blocking due to unused variables
+      "react/no-unescaped-entities": "off", // Prevents issues with apostrophes in JSX
+      "react/jsx-key": "warn", // Ensures missing keys don't block builds
+    },
+  },
 ];
 
 export default eslintConfig;
